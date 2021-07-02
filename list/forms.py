75a7dbb0a27022e06319cf01d0a1a91d6449c1b5
ebirtodaso <edwardkb@hawaii.edu>
2021-07-02 01:36:05 -1000

    corrected errors in some files

--- list/forms.py
+++ list/forms.py
@@ -1,18 +1,15 @@
-from django.urls import path
-from . import views
+from django import forms
+from .models import listItem
 
-app_name = 'lists'
+class doneForm(forms.Form):
+  isDone = forms.BooleanField(required=False)
 
-urlpatterns = [
-    path('', views.IndexView.as_view(extra_context={'addItem': None}), name='index'),
-    path('new', views.CreateListView.as_view(), name='create'),
-    path('additem', views.AddItemView.as_view(), name='additem'),
-    path('update/<int:pk>', views.UpdateView.as_view(), name='update'),
-    path('update/<int:list_id>/<int:pk>', views.UpdateItemView.as_view(), name='updateitem'),
-    path('delete/<int:pk>', views.DeleteView.as_view(), name='delete'),
-    path('delete/<int:list_id>/<int:pk>', views.DeleteItemView.as_view(), name='deleteitem'),
-    path('<int:pk>/done', views.DoneView, name='done'),
-    path('register/', views.register, name='register'),
-    path('logout', views.logout_request, name='logout'),
-    path('login/', views.login_request, name='login'),
-]
+  class Meta:
+    model = listItem
+    exclude = ['item']
+
+class addItemForm(forms.Form):
+
+  class Meta:
+    model = listItem
+    fields = ('item')
